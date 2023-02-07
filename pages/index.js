6767ac4b0a223b00bf8c69147a55ae652ac04049
Aalind Sharma <aalinds.sharma@gmail.com>
2023-02-07 18:18:29 +0530

    added blog titles on homepage

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 
-export default function Home() {
+function Home({ posts }) {
   return (
     <Layout home>
       <Head>
@@ -11,6 +11,29 @@ export default function Home() {
       <section className={utilStyles.headingMd}>
         <p>hello I am Aalind a NextJS developer. You can contact me on my Twitter.</p>
       </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {posts.map(({ id, title, body }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+            </li>
+          ))}
+        </ul>
+      </section>
     </Layout>
   );
+}
+
+export default Home;
+
+export async function getStaticProps() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await response.json();
+
+  return {
+    props: {
+      posts: data,
+    },
+  };
 }
