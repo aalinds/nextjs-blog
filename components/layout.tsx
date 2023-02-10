1dc066e26589e871b1f7da26e55d8592ecb95eab
Aalind Sharma <aalinds.sharma@gmail.com>
2023-02-10 14:59:03 +0530

    added typescript

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,13 +1,14 @@
+import * as React from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'Aalind Sharma';
-export const siteTitle = 'Next.js Sample Website';
+const name: string = 'Aalind Sharma';
+export const siteTitle: string = 'Next.js Sample Website';
 
-export default function Layout({ children, home }) {
+export default function Layout({ children, home }: { children: React.ReactNode; home?: boolean }) {
   return (
     <div className={styles.container}>
       <Head>
